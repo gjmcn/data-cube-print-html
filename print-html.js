154b8491b1be1e22b543aa2af00730187c52d3ae
gmcn <gjmcn@users.noreply.github.com>
2018-05-12 15:48:02 +0100

    print and info working

--- print-html.js
+++ print-html.js
@@ -1,38 +1,31 @@
-
-//NOTES:
-  //use document.body.contains to see if elmt exists
-  // check using eg _l not this._l
-
 {
   'use strict';
   
   const helper = require('data-cube-helper');
-  const {addArrayMethod, polarize, assert, dimName} = helper;
+  const {addArrayMethod, polarize, assert, dimName, def} = helper;
       
   //change these here for now
-  const dp = 4;           //decimal places
   const maxPrint = 2000;  //show info rather than table if more entries
   const maxStr = 40;      //tuncate strings if longer
-  const maxMag = 1e6;     //use scientific notation if x >= maxMag or
-                          //x < 10^(-maxMag)
-
+  const maxMag = 1e6;     //use scientific notation if x >= maxMag
+  const dp = 4;           //decimal places (use sci notation if abs(x) < 10^(-dp))
   
   //--------------- auxiliary ---------------//
   
-  //str[, array, array] -> str, wraps val in given tag type.
-  //  cls: if passed, a class is added for each entry
-  //  rcp: if passed, the 3 entries are included as values of
+  //str, str[, str, array] -> str, wraps val in given tag type.
+  //  cls: if truthy, added as class attribute
+  //  rcp: if truthy, the 3 entries are included as values of
   //       the attributes data-row, data-col and data-page
   const elm = (tag, val, cls, rcp) => {
     s = `<${tag}`;
-    if (cls) s += ` class="${cls.join(' ')}"`;
+    if (cls) s += ` class="${cls}"`;
     if (rcp) s += ` data-row="${rcp[0]}" data-col="${
                     rcp[1]}" data-page="${rcp[2]}"`;
     return `${s}>${val}</${tag}>`;
   };
-  
+    
   //convenience func for wrapping val in <td> tag
-  const td = (val, cts, rcp) => elm('td', val, cts, rcp);
+  const td = (val, cls, rcp) => elm('td', val, cls, rcp);
 
   //truncate string if longer than max
   const truncate = (s,mx) => s.length > mx ? s.slice(0,mx) + ' ...' : s;
@@ -40,25 +33,24 @@
   //format functions
   let fmt = {
     number: x => {
-      if (Number.isInteger(x)) return = '' + x;
+      if (Number.isInteger(x)) return '' + x;
       else if (Math.abs(x) >= maxMag || Math.abs(x) < Math.pow(10,-dp)) {
         return (+x.toPrecision(dp)).toExponential();
       }
       else return x.toFixed(dp);
     },
-    string: x => `'${truncate(x,maxStr)}'`,
+    string: x => truncate(x,maxStr),
     boolean: x => x,
-    null: () => 'null',
     undefined: () => 'undefined',
-    function: x => 'function',    
+    function: () => 'function',
+    null: () => 'null',
+    cube: x => 'cube (entries: ' + x.length + ', shape: ' + x._s.join(', ') + ')',
+    array: x => 'array (entries: ' + x.length + ')',
     date: x => ('' + x).replace(/\s*\([^\)]*\)/, ''),
-    cube: x => 'cube (entries: ' + x.length + ', shape: ' + x._s + ')',
-    array: x => 'array (entries: ' + x.length +')',
     other: x => truncate('' + x, maxStr),
     index: x => '(' + x + ')',
     key: x => truncate('' + x, maxStr),
     label: x => x,
-    faint: x => x   //!! SHOULD NOT NEED THIS HERE?????
   };
   
   //entry format function
@@ -74,72 +66,83 @@
     if (x instanceof Date) return [fmt.date(x), 'date'];
     return [fmt.other(x), 'other'];
   };
+  
+  //array/cube, str[, *, str, bool] => array/cube/DOM-elmt
+  //create div, set its innerHTML to s, print and return
+  //based on the options to, id and chain (see print)
+  const displayReturn = (x, s, to, id, chain) => {
+    const dv = document.createElement('div');
+    if (id !== undefined) dv.id = id;
+    dv.innerHTML = s;
+    dv.className = 'data-cube-wrap';
+    if (to) {
+      if (typeof to === 'string') {
+        document.querySelector(to).insertBefore(dv,null);
+      }
+      else if (to instanceof Element) to.insertBefore(dv,null);
+      else if (to[0] instanceof Element) to[0].insertBefore(dv,null);
+    }
+    return chain ? x : dv;
+  }
 
   
   //--------------- print cube or standard array ---------------//
-
+  
   //MOVE MOST DETAILS TO README
   //[object] -> cube/str
-  //element:
-  //  -element:
+  //options object:
+  //  -to:  
   //    -falsy (default): prints nothing
-  //    -string: passed to document.querySelectorAll; table(s) appended
-  //     to first element in selection
-  //    -otherwise, checks if value is in DOM, if not, checks if val[0]
-  //     in DOM; prints to elmt it finds
-  
-  HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-  SINCE CAN HAVE MULTIPLE TABLES, ALWAYS WRAP IN DIV - THIS IS THE ELMT THAT RETURN
-    -SHOULD TABLE ELMTS BE GIVEN WAY TO IDENTIFY THEM - INDEX OR KEY>
-  
-  //  -ret:
-  //    -'this' (default), the array/cube
-  //    -'string', HTML string for the table(s)
-  //    -'table', the table element
-  //  -indexKey: array/singleton, the indices/keys of a dimension are printed
-  //     if the corresponding entry is truthy, singleton is broadcast
+  //    -string: passes string to document.querySelector and appends
+  //     printed array/cube to selected element
+  //    -otherwise: if is an element, appends to it; else if property
+  //     at index 0 is an elmt, appends to it
+  //  -chain: if truthy, returns the array/cube; if falsy (default) returns
+  //    the wrapper div containing the printed array/cube
   //  -label: array/singleton, label of a dimension is printed if the
-  //     corresponding entry is truthy (and the label exists) singleton is
-  //     broadcast
+  //   corresponding entry is truthy (and the label exists), a  singleton
+  //   is broadcast. Default: labels are printed if they exist
+  //  -indexKey: array/singleton, the indices/keys of a dimension are
+  //   printed if the corresponding entry is truthy, singleton is broadcast.
+  //   Default: row and col indices/keys are printed; page inds/keys are
+  //   printed if there are multiple pages or there are page keys or there
+  //   is a page label that is being printed
   //  -type: if truthy, <td>s representing entries have a class
-  //    indicating their type
+  //   indicating their type
   //  -entry: if truthy, <td>s representing entries have a attributes
-  //    data-row, data-col, data-page; values are indices/keys (keys
-  //    are converted to strings, but not formatted)
-  //Notes:
-  //  -an array is printed as a cube, but the array is NOT converted
-  //   to a cube.
-  //  -if neither the indexKey nor label options are used:
-  //    -row and col inds/keys are printed
-  //    -page inds/keys are printed when there are multiple pages or
-  //     the cube has page keys or a page label
-  //    -labels are printed where they exist
-  addArrayMethod('print', function(ops)) {
+  //   data-row, data-col, data-page; values are indices/keys (keys
+  //   are converted to strings, but not formatted)
+  //  -table: if truthy, <table> for each page has data-table attribute
+  //   with the page index/key as the value (keys are converted to strings,
+  //   but not formatted)
+  //  -id: id for wrapper div. Default: no id used
+  //Note: an array is printed as a cube, but the array itself is NOT
+  //converted to a cube.
+  addArrayMethod('print', function(ops) {
                  
     const shortDimName = ['row', 'col', 'page'];             
-                 
+    
+    //ensure _k and _l are arrays so can look up entry to
+    //see if keys/label exists
     let _s, _k, _l;
-    if (this._data_cube) {_s, _k, _l} = this;
-    else [_s, _k, _l] = [[this.length, 1, 1], [], []];
+    if (this._data_cube) ({_s, _k, _l} = this);
+    else _s = [this.length, 1, 1];
+    _k = _k || [];
+    _l = _l || [];
     const [nr, nc, np] = _s;
                  
     //process options
     ops = def(assert.single(ops), {});
-    if (typeof ops !== 'object') throw Error('print options: object expected');
-    let {element, ret, indexKey, label, type, entry} = ops;
+    if (typeof ops !== 'object') throw Error('object expected');
+    let {to, chain, label, indexKey, type, entry, table, id} = ops;
     let indexKeySingle, labelSingle;
-    //ret
-    if (ret === undefined) ret = 'this';
-    else if (ret !== 'this' && ret !== 'string' && ret !== 'table') {
-      throw Error(`'this', 'string', or 'table' expected`);
-    }
     //labels
     [label,labelSingle] = polarize(label); 
     if (labelSingle) label = (label === undefined) ? [1, 1, 1] : [label, label, label];
     else if (label.length !== 3) {
       throw Error('label option: singleton or 3-entry array expected');
     }
-    label = label.map(a d => a && _l[d]);  //only if label exists (never for array)
+    label = label.map((a,d) => a && _l[d]);
     //indices/keys
     [indexKey,indexKeySingle] = polarize(indexKey); 
     if (indexKeySingle) {
@@ -151,36 +154,40 @@
     else if (indexKey.length !== 3) {
       throw Error('indexKey option: singleton or 3-entry array expected');
     }
-    //get keys as strings if needed
-    const keyStr = indexKey.map( (ik,d) => { 
+  
+    //use info if empty or too many entries
+    if (this.length > maxPrint || this.length === 0) return this.info({to,chain,id});
+  
+    //need some/all keys as strings if printing keys or if entry or table option is truthy
+    const keyStr = indexKey.map( (ik,d) => {
       if (_k[d]) {
-        if (entry || ik) {
+        const attr = entry || (d === 2 && table);  //adding inds/keys for this dim as attributes
+        if (ik || attr) {
           let a = Array.from(_k[d].keys(), ky => '' + ky);
-          if (entry && new Set(a).size !== _s[d]) {
-            throw Error(`entry attributes: ${helper.dimName[d]} keys do not convert to unique strings`);
+          if (attr && new Set(a).size !== _s[d]) {
+            throw Error(`data attributes: ${dimName[d]} keys do not convert to unique strings`);
           }
           return a;
         }
       }
     });
   
-    //use info if empty or too many entries
-    if (this.length > maxPrint || this.length === 0) return this.info(ops);
-    
     //label elements
     let rl, cl, pl;
-    if (label[0]) rl = `<td class="${row-label}" rowspan="${nr}">${fmt.label(_l[0])}</td>`;
-    if (label[1]) cl = `<td class="${col-label}" rowspan="${nc}">${fmt.label(_l[1])}</td>`;
-    if (label[2]) pl = `<span class="${page-label}">${fmt.label(_l[2])}</span>`;
+    if (label[0]) rl = `<td class="row-label" rowspan="${nr}">${fmt.label(_l[0])}</td>`;
+    if (label[1]) cl = `<td class="col-label" colspan="${nc}">${fmt.label(_l[1])}</td>`;
+    if (label[2]) pl = `<span class="page-label">${fmt.label(_l[2])}</span>`;
 
     //index/key elements
-    let [rik, cik, pik] = _k.map( (ik,d) => {
-      if (indexKey[d]) {
-        const cls = [`${shortDimName[d]}-ik`];
-        if (_k[d]) return = keyStr[d].map(ky => td(fmt.key(ky), cls));
+    let [rik, cik, pik] = indexKey.map( (ik,d) => {
+      if (ik) {
+        const cls = `${shortDimName[d]}-ik`;
+        const tg = d === 2 ? 'span' : 'td';
+        if (_k[d]) return keyStr[d].map(ky => elm(tg, fmt.key(ky), cls));
         else {
-          let a = new Array(_s[d]);
-          for (let i=0; i<_s[d]; i++) a[i] = elm(d === 2 ? 'span' : 'td', fmt.index(i), cls);
+          const nd = _s[d];
+          const a = new Array(nd);
+          for (let i=0; i<nd; i++) a[i] = elm(tg, fmt.index(i), cls);
           return a;
         }
       }
@@ -190,18 +197,20 @@
     //offets and topLeft
     const rowOffset = 0 + !!label[1] + !!indexKey[1];
     const colOffset = 0 + !!label[0] + !!indexKey[0];
-    const topLeft = (rowOffset && colOffset)
-      ? `<td rowspan="${rowOffset}" colspan="${colOffset}"></td>`
+    const topLeft = rowOffset && colOffset
+      ? `<td class="top-left" rowspan="${rowOffset}" colspan="${colOffset}"></td>`
       : '';
     
     //pages
     let str = '';
     for (let p=0; p<np; p++) {
-      str += '<table>';
+      str += '<table class="page-table"';
+      if (table) str += ` data-table="${_k[2] ? keyStr[2][p] : p}"`;
+      str += '>';
       
       //page info
       if (pl || pik) {
-        str += `<caption class="${page-caption}">`;
+        str += '<caption class="page-caption">';
         if (pl) str += pl;
         if (pik) {
           if (pl) str += ': ';
@@ -211,62 +220,79 @@
       }
       
       //col label and inds/keys
-      if (topLeft) str += '<tr>' + topLeft;
+      str += '<tr>';
+      if (topLeft) str += topLeft;
       if (cl) str += cl + '</tr>';
       if (cik) {
         if (cl) str += '<tr>';
         str += cik + '</tr>';
       }
       
-      //row label, inds/keys and entries
+      //row label, row inds/keys and entries
       if (rl) str += '<tr>' + rl;
       for (let r=0; r<nr; r++) {
         if (!(r === 0 && rl)) str + '<tr>';
         if (rik) str += rik[r];
         for (let c=0; c<nc; c++) {
           let [ent,typ] = fmtEntry(this[r + nr*c + nr*nc*p]);
-          str += td( ent, type ? typ : undefined, entry ? entryAttr(r,c,p) : undefined);
+          str += td(
+            ent,
+            type ? typ : undefined,
+            entry ? 
+              [
+                keyStr[0] ? keyStr[0][r] : r,
+                keyStr[1] ? keyStr[1][c] : c,
+                keyStr[2] ? keyStr[2][p] : p
+              ]
+              : undefined
+          );
         }
         str += '</tr>';
       }
       str += '</table>';
     }
-      
+    
     //print and return
-    if (retStr) return str;
-    console.log(str);
-    return this;
+    return displayReturn(this, str, to, id, chain);
+  
   });
-  
-------------------------DRAFT DONE TO HERE----------------------------------
 
-  
+
   //--------------- print info on cube or standard array ---------------//
   
-  addArrayMethod('info', function(retStr) {
-    helper.assert.single(retStr);
-    let str;
+  //As in print, ops is an options object, but only the to,
+  //chain, and id properties are used.
+  addArrayMethod('info', function(ops) {
+    ops = def(assert.single(ops), {});
+    if (typeof ops !== 'object') throw Error('object expected');
+    let {to, chain, id} = ops;
+    let str = '<table class="info-table">';
     if (this._data_cube) {
       const keys = [];
       const labels = [];
-      if (this._k) this._k.forEach((a,i) => a ? keys.push(dimName[i]) : 0);
-      if (this._l) this._l.forEach((a,i) => a ? labels.push(dimName[i]): 0);
-      str =
-        '\n  entries:  ' + this.length + 
-        '\n  shape:    ' + this._s +
-        '\n  keys:     ' + (keys.length   ? keys   : '(none)') + 
-        '\n  labels:   ' + (labels.length ? labels : '(none)') +
-        '\n';
+      if (this._k) this._k.forEach((a,i) => a ?   keys.push(dimName[i]) : 0);
+      if (this._l) this._l.forEach((a,i) => a ? labels.push(dimName[i]) : 0);
+      str +=
+        `<tr><td>entries:</td><td>${this.length}</td></tr>
+         <tr><td>shape:</td><td>${this._s.join(', ')}</td></tr>
+         <tr><td>keys:</td><td>${keys.length ? keys.join(', ') : '(none)'}</td></tr>
+         <tr><td>labels:</td><td>${labels.length ? labels.join(', ') : '(none)'}</td></tr>
+         </table>`;
     }
-    else str = '\n  (standard array)\n  entries: ' + this.length + '\n';
-    if (retStr) return str;
-    console.log(str);
-    return this;
+    else {
+      str +=
+        `<tr><td>(standard array)</td></tr>
+         <tr><td>entries: ${this.length}</td></tr>
+         </table>`;
+    }
+    return displayReturn(this, str, to, id, chain);
   });
   
   
   //--------------- export function to set compact ---------------//
   
+  // HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
   module.exports = ops => {
     if (ops.hasOwnProperty('compact')) printOps.compact = ops.compact;
   }
