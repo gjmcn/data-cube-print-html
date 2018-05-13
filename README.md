75bdfd898d099e4429591920dbdbeba771f07bbd
gmcn <gjmcn@users.noreply.github.com>
2018-05-13 15:55:50 +0100

    add example

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# data-cube-print-html
+Print [DataCubes](https://github.com/gjmcn/data-cube) in HTML documents:
+
+* Cubes (or standard arrays) are printed as HTML tables.
+
+* Each entry, index/key and label of a cube is passed through a format function:
+	* There are seperate format functions for row labels, row keys, column labels etc.
+	* For entries, each type has its own format function.
+	* Any format function can be changed or reset to its default.
+
+
+HERE!!!!!!!!!!!!!!
+
+* Tables are styled using CSS or JavaScript. Table elements are given intutitive class names to facilitate this. Furthermore, there is an opton to give data attributes so any entry can be found by its indices/keys.
+
+
+
+
+
+
+## Usage
+
+DO THIS WHEN DECIDE HOW WILL PACKAGE
+
+## 
+
+## Notes
+
+* `print` and `info` can be used to print intermediate results, e.g. `x.sum().print().sqrt()`.
+
+
+* `print` and `info` do *not* convert a standard array to a cube like core DataCube methods do
+
+
+* this package cannot currently be used in the browser; use [data-cube-print-html](https://github.com/gjmcn/data-cube-print-html) to print cubes as HTML tables
+
--- print-html.js
+++ print-html.js
@@ -4,11 +4,6 @@
   const helper = require('data-cube-helper');
   const {addArrayMethod, polarize, assert, dimName, def} = helper;
       
-  //change these here for now
-  const maxPrint = 2000;  //show info rather than table if more entries
-  const maxStr = 40;      //tuncate strings if longer
-  const maxMag = 1e6;     //use scientific notation if x >= maxMag
-  const dp = 4;           //decimal places (use sci notation if abs(x) < 10^(-dp))
   
   //--------------- auxiliary ---------------//
   
@@ -30,35 +25,53 @@
   //truncate string if longer than max
   const truncate = (s,mx) => s.length > mx ? s.slice(0,mx) + ' ...' : s;
 
-  //format functions
-  let fmt = {
+  //* -> str, basic string formatting
+  const basicStrFmt = x => truncate('' + x, fmt.maxStr).replace(/\n/g, '<br>');
+  
+  //default format values and functions
+  const defFmt = {
+    
+    maxPrint: 2000,  //show info rather than array/cube if more entries
+    maxStr: 40,      //tuncate strings if longer
+    maxMag: 1e6,     //use scientific notation if x >= maxMag
+    dp: 4,           //decimal places (use sci notation if abs(x) < 10^(-dp))
+    
     number: x => {
       if (Number.isInteger(x)) return '' + x;
-      else if (Math.abs(x) >= maxMag || Math.abs(x) < Math.pow(10,-dp)) {
-        return (+x.toPrecision(dp)).toExponential();
+      else if (Math.abs(x) >= fmt.maxMag || Math.abs(x) < Math.pow(10,-fmt.dp)) {
+        return (+x.toPrecision(fmt.dp)).toExponential();
       }
-      else return x.toFixed(dp);
+      else return x.toFixed(fmt.dp);
     },
-    string: x => truncate(x,maxStr),
+    string: basicStrFmt,
     boolean: x => x,
-    undefined: () => 'undefined',
-    function: () => 'function',
-    null: () => 'null',
+    undefined: x => 'undefined',
+    function: x => 'function',
+    null: x => 'null',
     cube: x => 'cube (entries: ' + x.length + ', shape: ' + x._s.join(', ') + ')',
     array: x => 'array (entries: ' + x.length + ')',
     date: x => ('' + x).replace(/\s*\([^\)]*\)/, ''),
-    other: x => truncate('' + x, maxStr),
-    index: x => x,
-    key: x => truncate('' + x, maxStr),
-    label: x => x //'(' + x + ')'
+    other: basicStrFmt,
+    rowIndex:  x => x,
+    colIndex:  x => x,
+    pageIndex: x => x,
+    rowKey:  basicStrFmt,
+    colKey:  basicStrFmt,
+    pageKey: basicStrFmt,
+    rowLabel:  basicStrFmt,
+    colLabel:  basicStrFmt,
+    pageLabel: basicStrFmt
   };
+  
+  //format functions used by print()
+  let fmt = Object.assign({}, defFmt);
   
   //entry format function
   const basicType = new Set(['number', 'string', 'boolean', 'undefined', 'function']);
   const fmtEntry = x => {
     const t = typeof x;
     if (basicType.has(t)) return [fmt[t](x), t];
-    if (x === null) return [fmt.null(), 'null'];
+    if (x === null) return [fmt.null(x), 'null'];
     if (Array.isArray(x)) {
       if (x._data_cube) return [fmt.cube(x), 'cube'];
       else return [fmt.array(x), 'array'];
@@ -66,7 +79,7 @@
     if (x instanceof Date) return [fmt.date(x), 'date'];
     return [fmt.other(x), 'other'];
   };
-  
+    
   //array/cube, str[, *, str, bool] => array/cube/DOM-elmt
   //create div, set its innerHTML to s, print and return
   //based on the options to, id and chain (see print)
@@ -108,13 +121,13 @@
   //   printed if there are multiple pages or there are page keys or there
   //   is a page label that is being printed
   //  -type: if truthy, <td>s representing entries have a class
-  //   indicating their type
-  //  -entry: if truthy, <td>s representing entries have a attributes
+  //   indicating their type. Default: true
+  //  -entryAttr: if truthy, <td>s representing entries have attributes
   //   data-row, data-col, data-page; values are indices/keys (keys
-  //   are converted to strings, but not formatted)
-  //  -table: if truthy, <table> for each page has data-table attribute
+  //   are converted to strings, but not formatted). Default: false.
+  //  -tableAttr: if truthy, <table> for each page has data-table attribute
   //   with the page index/key as the value (keys are converted to strings,
-  //   but not formatted)
+  //   but not formatted). Default: false
   //  -id: id for wrapper div. Default: no id used
   //Note: an array is printed as a cube, but the array itself is NOT
   //converted to a cube.
@@ -134,7 +147,8 @@
     //process options
     ops = def(assert.single(ops), {});
     if (typeof ops !== 'object') throw Error('object expected');
-    let {to, chain, label, indexKey, type, entry, table, id} = ops;
+    let {to, chain, label, indexKey, type, entryAttr, tableAttr, id} = ops;
+    type = def(type, true);
     let indexKeySingle, labelSingle;
     //labels
     [label,labelSingle] = polarize(label); 
@@ -156,12 +170,12 @@
     }
   
     //use info if empty or too many entries
-    if (this.length > maxPrint || this.length === 0) return this.info({to,chain,id});
+    if (this.length > fmt.maxPrint || this.length === 0) return this.info({to,chain,id});
   
-    //need some/all keys as strings if printing keys or if entry or table option is truthy
+    //need some/all keys as strings if printing keys or if entryAttr/tableAttr is truthy
     const keyStr = indexKey.map( (ik,d) => {
       if (_k[d]) {
-        const attr = entry || (d === 2 && table);  //adding inds/keys for this dim as attributes
+        const attr = entryAttr || (d === 2 && tableAttr);  //adding inds/keys for this dim as attributes
         if (ik || attr) {
           let a = Array.from(_k[d].keys(), ky => '' + ky);
           if (attr && new Set(a).size !== _s[d]) {
@@ -174,20 +188,24 @@
   
     //label elements
     let rl, cl, pl;
-    if (label[0]) rl = `<td class="label row-label" rowspan="${nr}">${fmt.label(_l[0])}</td>`;
-    if (label[1]) cl = `<td class="label col-label" colspan="${nc}">${fmt.label(_l[1])}</td>`;
-    if (label[2]) pl = `<span class="label page-label">${fmt.label(_l[2])}</span>`;
+    if (label[0]) rl = `<td class="label row-label" rowspan="${nr}">${fmt.rowLabel(_l[0])}</td>`;
+    if (label[1]) cl = `<td class="label col-label" colspan="${nc}">${fmt.colLabel(_l[1])}</td>`;
+    if (label[2]) pl = `<span class="label page-label">${fmt.pageLabel(_l[2])}</span>`;
 
     //index/key elements
     let [rik, cik, pik] = indexKey.map( (ik,d) => {
       if (ik) {
         const cls = `ik ${shortDimName[d]}-ik`;
         const tg = d === 2 ? 'span' : 'td';
-        if (_k[d]) return keyStr[d].map(ky => elm(tg, fmt.key(ky), cls));
+        if (_k[d]) {
+          const fmtKey = fmt[shortDimName[d] + 'Key'];
+          return keyStr[d].map(ky => elm(tg, fmtKey(ky), cls));
+        }
         else {
+          const fmtIndex = fmt[shortDimName[d] + 'Index'];
           const nd = _s[d];
           const a = new Array(nd);
-          for (let i=0; i<nd; i++) a[i] = elm(tg, fmt.index(i), cls);
+          for (let i=0; i<nd; i++) a[i] = elm(tg, fmtIndex(i), cls);
           return a;
         }
       }
@@ -205,7 +223,7 @@
     let str = '';    
     for (let p=0; p<np; p++) {
       str += '<table class="page-table"';
-      if (table) str += ` data-table="${_k[2] ? keyStr[2][p] : p}"`;
+      if (tableAttr) str += ` data-table="${_k[2] ? keyStr[2][p] : p}"`;
       str += '>';
       
       //page info
@@ -238,7 +256,7 @@
           str += td(
             ent,
             'entry' + (type ? ` ${typ}` : ''),
-            entry ? 
+            entryAttr ? 
               [
                 keyStr[0] ? keyStr[0][r] : r,
                 keyStr[1] ? keyStr[1][c] : c,
@@ -289,13 +307,17 @@
   });
   
   
-  //--------------- export function to set compact ---------------//
-  
-  // HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //--------------- export function to change format functions ---------------//
 
-  module.exports = ops => {
-    if (ops.hasOwnProperty('compact')) printOps.compact = ops.compact;
-  }
+  module.exports = newFmt => {
+    newFmt = assert.single(newFmt);
+    if (newFmt === undefined) {
+      fmt = Object.assign({}, defFmt);
+    }
+    else {
+      if (typeof newFmt !== 'object') throw Error('object expected');
+      fmt = Object.assign(fmt, newFmt);
+    }
+  };
 
 }
-
